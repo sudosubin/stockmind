0f39d747f73435c4c1a90f250237d72b9a818e26
sudosubin <sudosubin@gmail.com>
2025-02-14 21:10:10 +0900

    feat: add nix flake files

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "openapi-python";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      inherit (nixpkgs.lib) genAttrs platforms;
+      forAllSystems = f: genAttrs platforms.unix (system: f (import nixpkgs { inherit system; }));
+
+    in
+    {
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            bun
+          ];
+        };
+      });
+    };
+}
