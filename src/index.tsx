bcbdabf9b1baf6fd8e8de51f1cf7b92e981ff339
sudosubin <sudosubin@gmail.com>
2025-02-14 21:42:26 +0900

    feat: add reshaped

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,25 @@
 import { render } from "preact";
-import { LocationProvider, Router, Route } from "preact-iso";
+import { LocationProvider, Route, Router } from "preact-iso";
 
 import { Header } from "./components/Header.jsx";
 import { Home } from "./pages/Home/index.jsx";
 import { NotFound } from "./pages/_404.jsx";
-import "./style.css";
+
+import "reshaped/themes/reshaped/theme.css";
+import { Reshaped } from "reshaped";
 
 export function App() {
   return (
     <LocationProvider>
-      <Header />
-      <main>
+      <Reshaped theme="reshaped" defaultColorMode="light">
+        <Header />
         <Router>
           <Route path="/" component={Home} />
           <Route default component={NotFound} />
         </Router>
-      </main>
+      </Reshaped>
     </LocationProvider>
   );
 }
 
-render(<App />, document.getElementById("app"));
+render(<App />, document.getElementById("root"));
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,4 @@
 import preactLogo from "../../assets/preact.svg";
-import "./style.css";
 
 export function Home() {
   return (
