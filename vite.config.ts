33a401252c67e4148d4490ce0e576167fdafbf12
sudosubin <sudosubin@gmail.com>
2025-02-14 21:45:47 +0900

    feat: add vanilla-extract

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import preact from "@preact/preset-vite";
+import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [preact()],
+  plugins: [preact(), vanillaExtractPlugin()],
 });
