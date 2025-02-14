33ba0cf975ae9bbab6742dc911b11c5d70f08247
sudosubin <sudosubin@gmail.com>
2025-02-14 22:23:41 +0900

    feat: remove default page and components from template

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,6 @@
 import { render } from "preact";
 import { LocationProvider, Route, Router } from "preact-iso";
 import { Reshaped } from "reshaped";
-import { Header } from "#/components/Header.jsx";
-import { Home } from "#/pages/Home/index.jsx";
-import { NotFound } from "#/pages/_404";
 import "#/shared/modern-normalize.css";
 import "reshaped/themes/reshaped/theme.css";
 
@@ -11,10 +8,9 @@ export function App() {
   return (
     <LocationProvider>
       <Reshaped theme="reshaped" defaultColorMode="light">
-        <Header />
         <Router>
-          <Route path="/" component={Home} />
-          <Route default component={NotFound} />
+          {/* <Route path="/" component={Home} />
+          <Route default component={NotFound} /> */}
         </Router>
       </Reshaped>
     </LocationProvider>
