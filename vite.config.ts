38ebb0fa98109ffed0d8c0ae5b0e619d6a55bf42
sudosubin <sudosubin@gmail.com>
2025-03-14 22:55:08 +0900

    feat: use vite-plugin-html to minify

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 import preact from "@preact/preset-vite";
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 import { defineConfig } from "vite";
+import { createHtmlPlugin } from "vite-plugin-html";
 
 export default defineConfig({
   build: { minify: "terser" },
   esbuild: { legalComments: "external" },
-  plugins: [preact(), vanillaExtractPlugin()],
+  plugins: [
+    createHtmlPlugin({ minify: true }),
+    preact(),
+    vanillaExtractPlugin(),
+  ],
   resolve: { alias: { lodash: "lodash-es", wouter: "wouter-preact" } },
 });
